2103306ec168d6b907094d2ef2cc2458b02e4314
Jenna <jennabsol@gmail.com>
2019-11-06 07:48:15 -0800

    first commit

---
+++ BangazonAPI/Models/Employee.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BangazonAPI.Models
+{
+    public class Employee
+    {
+      
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public bool IsSuperVisor { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        public Department department { get; set; }
+
+
+    }
+}
---
+++ BangazonAPI/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BangazonAPI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            // For serving the HTML, CSS and JavaScript for a client web app.
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // For routing to API controller actions.
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
